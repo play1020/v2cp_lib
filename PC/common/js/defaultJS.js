936750abc5e8ed03453e2329f30a4dee310a2194
play1020 <play10202000@gmail.com>
2018-12-17 15:59:41 +0900

    다이얼로그창 스크립트제작
    다이얼로그(레이어팝업) 기본 기능에 추가

--- PC/common/js/defaultJS.js
+++ PC/common/js/defaultJS.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 $(document).ready(function(){
 	//닫기
-	$('.close').on('click',function(){
+	$(document).on('click','.close',function(){
 		var tar = String($(this).attr('data-target'));
-		$('.'+tar).slideUp('fast');
+		$('.'+tar).hide();
 	});
 	
 	//메인메뉴
@@ -14,4 +14,19 @@ $(document).ready(function(){
 		$('#header .lnb_wrap .lnb > li > ul').stop().fadeOut('fast');
 		$('.lnb_2depth').stop().slideUp('fast');
 	});
+	
+	//레이어팝업
+	$('[data-dialogCall]').on('click',function(){
+		//오픈할 다이얼로그창
+		var callName = $(this).attr('data-dialogCall');
+		$('[data-dialog='+callName+']').fadeIn('fast');
+		//닫기버튼 이벤트 입력
+		$('[data-dialog='+callName+']').find('.close').on('click',function(){
+			dialogWindowClose(callName)
+		})
+	})
+	function dialogWindowClose(target){
+		$('[data-dialog='+target+']').fadeOut('fast');
+	}
+	
 });
