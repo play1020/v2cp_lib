b63e42bb6ee41b566827e8ef6c84ecedd0b6c999
play1020 <play10202000@gmail.com>
2020-10-26 09:28:00 +0900

    컴포넌트추가 페럴렉스

--- PC/common/js/defaultJS.js
+++ PC/common/js/defaultJS.js
@@ -31,4 +31,16 @@ $(document).ready(function(){
 		$('[data-dialog='+target+']').fadeOut('fast');
 	}
 	
+	//페럴렉스 스크롤 배경
+	$('[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		$(window).scroll(function() {
+			var yPos = -($(window).scrollTop() / $bgobj.data('speed')); 
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+		}); 
+	});
+	
 });
